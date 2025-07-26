6e79a2925b849cd3f6e9f0ee233eab33c477a9be
Manan Kumar <manan.kumar@redica.com>
2025-07-27 03:12:02 +0530

    added checkbox related interactions

--- cypress/e2e/ui/radioCheckbox.spec.js
+++ cypress/e2e/ui/radioCheckbox.spec.js
@@ -1,8 +1,22 @@
 describe("Interactions and verifications on Radio Button and Checkbox", () => {
-  it("should select the radio button and checkbox and verify the results", () => {
+  before(() => {
     cy.visit("https://testautomationpractice.blogspot.com/");
-
+  });
+  it("should select the radio button and checkbox and verify the results", () => {
     cy.get("#male").check().should("be.checked");
     cy.get("#female").check().should("be.checked");
+
+    cy.get("#sunday").check().should("be.checked");
+    cy.get("#monday").check().should("be.checked");
+    cy.get("#tuesday").check().should("be.checked");
+    cy.get("#wednesday").check().should("be.checked");
+
+    cy.get("#thursday").should("not.be.checked");
+    cy.get("#friday").should("not.be.checked");
+    cy.get("#saturday").should("not.be.checked");
+
+    cy.get("#sunday").uncheck().should("not.be.checked");
+    cy.get("#monday").uncheck().should("not.be.checked");
+    cy.get("#tuesday").uncheck().should("not.be.checked");
   });
 });
